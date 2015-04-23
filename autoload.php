1cb55b22514d29ef940c7880545c33e4c198e2cc
Rafael de Meira Teixeira <rafaeldemeirateixeira@gmail.com>
2015-04-23 17:44:24 -0300

    Removendo espaços

--- autoload.php
+++ autoload.php
@@ -1,20 +1,15 @@
 <?php
 function auto_carregamento($className){
     $file = str_replace('\\', '/', $className . '.php');
-
     $smarty = substr($file, 0, 6);
-
     if($smarty == "Smarty" && $file != "Smarty.php"){
         $file = strtolower($file);
     }
-
     if($file != "src.php"){
         require_once $file;
     }
-
     if(!class_exists($className)){
         throw new InvalidArgumentException("Classe inexistente - " . $className);
     }
 }
-
 spl_autoload_register('auto_carregamento');
