50604e7f0617df535a8964f3d47a04bab23ff19f
Rafael de Meira Teixeira <rafaeldemeirateixeira@gmail.com>
2015-04-23 23:59:40 -0300

    Alterações e adição de novos metodos - Alterado o nome da variável que recebe a instacia do PDO; - Adicionado os metodos selectData(), selectDataTotalRegister(), update() e delete(); - Alterado método insert();

--- app/model/PessoaTipoModel.php
+++ app/model/PessoaTipoModel.php
@@ -2,38 +2,10 @@
 
 class PessoaTipoModel extends DeiaModel{
 
-    private static $pessoaTipoModel = null;
+    public static $model = null;
 
     public function __construct(){
-        self::$pessoaTipoModel = DeiaModel::PDOInstance();
-    }
-
-    /**
-     *
-     * @param type $data
-     */
-    public static function insert($data){
-
-        if (count($data) != 1){
-            throw new InvalidArgumentException("Numero de parametros != 1");
-        }
-
-        $qwy = self::$pessoaTipoModel->prepare(
-            "INSERT INTO `pessoa_tipo` (
-                `tipo_nome`
-            )
-            VALUES (
-                ?
-            )"
-        );
-
-        if (is_array($data)){
-            if($qwy->execute($data)){
-                return true;
-            }
-        }
-
-        return false;
+        self::$model = DeiaModel::PDOInstance();
     }
 
     /**
@@ -41,7 +13,7 @@ class PessoaTipoModel extends DeiaModel{
      * @return type
      */
     public static function selectAll($cod){
-        $qwy = self::$pessoaTipoModel->prepare(
+        $qwy = self::$model->prepare(
             "
                 SELECT
                     PT.tipo_cod AS value,
@@ -61,6 +33,110 @@ class PessoaTipoModel extends DeiaModel{
         );
         if($qwy->execute(array($cod))){
             return $qwy->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        return false;
+    }
+
+    public static function selectData($start, $max){
+        $qwy = self::$model->prepare(
+            "
+            SELECT tipo_cod, tipo_nome
+            FROM pessoa_tipo
+            ORDER BY tipo_nome
+            LIMIT $start, $max
+            "
+        );
+        if($qwy->execute()){
+            return $qwy->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+    public static function selectDataTotalRegister(){
+        $qwy = self::$model->prepare(
+            "
+            SELECT COUNT(tipo_cod) AS total
+            FROM pessoa_tipo
+            "
+        );
+        if($qwy->execute()){
+            $result = $qwy->fetch(PDO::FETCH_ASSOC);
+            return $result["total"];
+        }
+    }
+
+    public static function selectDataEdit($tipo_cod){
+        $qwy = self::$model->prepare(
+            "
+            SELECT tipo_cod, tipo_nome
+            FROM pessoa_tipo
+            WHERE tipo_cod = ?
+            "
+        );
+        if($qwy->execute(array($tipo_cod))){
+            return $qwy->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+
+    /**
+     *
+     * @param type $data
+     */
+    public static function insert($data){
+
+        if (count($data) != 1){
+            throw new InvalidArgumentException("Numero de parametros != 1");
+        }
+
+        $qwy = self::$model->prepare(
+            "INSERT INTO `pessoa_tipo` (
+                `tipo_nome`
+            )
+            VALUES (
+                ?
+            )"
+        );
+
+        if (is_array($data)){
+            if($qwy->execute($data)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     * @param type $data
+     */
+    public static function update($tipo_cod, $tipo_nome){
+
+        $qwy = self::$model->prepare(
+            "
+                UPDATE pessoa_tipo SET
+                    tipo_nome = ?
+                WHERE
+                    tipo_cod = ?
+            "
+        );
+
+        if($qwy->execute(array($tipo_nome, $tipo_cod))){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function delete($tipo_cod){
+        $qwy = self::$model->prepare(
+            "
+                DELETE FROM pessoa_tipo WHERE tipo_cod = ?
+            "
+        );
+
+        if($qwy->execute(array($tipo_cod))){
+            return true;
         }
 
         return false;
