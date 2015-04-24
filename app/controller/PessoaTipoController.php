889763bb3b50a3ba822438d846b5767a80a07bb6
Rafael de Meira Teixeira <rafaeldemeirateixeira@gmail.com>
2015-04-23 23:52:45 -0300

    Adicionado metodos e parametros - Adicionado botões Add e List; - Substituido o metodo Form() por Grid() no Index; - Alterado o nome do metodo Form() para Add(); - Criado metodo Edit, Delete e SalvarEdit; - Alterado o metodo Grid() para receber paginação e icones de interação;

--- app/controller/PessoaTipoController.php
+++ app/controller/PessoaTipoController.php
@@ -19,6 +19,11 @@ class PessoaTipoController extends DeiaController{
         $this->template->assign("caption","Cadastro de Tipos");
         $this->template->assign("subcaption","Destina-se ao cadastro de tipos de pessoa. Ex: Membros, Agregados, etc...");
 
+        //Habilita os botoes para adicionar e listar dados
+        $action["add"] = array("enable"=>true,"href"=>$this->urlController."Add/");
+        $action["list"] = array("enable"=>true,"href"=>$this->urlController."Grid/");
+        $this->template->assign("action", $action);
+
         new PessoaTipoModel();
     }
 
@@ -26,25 +31,62 @@ class PessoaTipoController extends DeiaController{
     public function index(){
 
         try {
-            $this->form();
+            $this->grid();
 
         } catch (SmartyException $e) {
             echo $e->getMessage();
         }
     }
 
-    public function form(){
+    public function add(){
         try{
-            $field[] =array("type"=>"text","name"=>"tipo_nome","label"=>"Tipo de Pessoa","required"=>true,"mensagem"=>"Favor informar o nome do tipo!");
+            $field[] = array("type"=>"text","name"=>"tipo_nome","label"=>"Tipo de Pessoa","required"=>true,"mensagem"=>"Favor informar o nome do tipo!");
 
             $this->template->assign("fields",$field);
-            $this->template->assign("form",array("name"=>"formulario","method"=>"post","action"=>$this->urlController . "Salvar"));
+            $this->template->assign("form",array("name"=>"formulario","method"=>"post","action"=>$this->urlController . "Salvar","button"=>"Salvar"));
             $form = $this->template->fetch("form.tpl");
 
-            $this->template->assign("content",$form);
-            $this->template->display("home.tpl");
+            $this->show($form);
+
         }catch(InvalidArgumentException $e){
             echo $e->getMessage();
+        }
+    }
+
+    public function edit($parametro){
+        try{
+            $edit = PessoaTipoModel::selectDataEdit($parametro[2]);
+
+            $field[] = array("type"=>"hidden","name"=>"tipo_cod","value"=>$edit["tipo_cod"]);
+            $field[] = array("type"=>"text","name"=>"tipo_nome","value"=>$edit["tipo_nome"],"label"=>"Tipo de Pessoa","required"=>true,"mensagem"=>"Favor informar o nome do tipo!");
+
+            $this->template->assign("fields",$field);
+            $this->template->assign("form",array("name"=>"formulario","method"=>"post","action"=>$this->urlController . "SalvarEdit","button"=>"Salvar"));
+            $form = $this->template->fetch("form.tpl");
+
+            $this->show($form);
+
+        }catch(InvalidArgumentException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    public function delete($parametro){
+        try{
+            $tipo_cod = $parametro[2];
+
+            $this->checkNumeric($tipo_cod);
+
+            if(PessoaTipoModel::delete($tipo_cod)){
+                $this->splash("Registro removido com sucesso");
+                $this->go($this->urlController);
+            }else{
+                $this->splash("Tentativa de remover falhou! Tente novamente");
+                $this->go($this->urlController);
+            }
+        }catch (PDOException $e){
+            $this->splash($e->getMessage());
+            $this->go($this->urlController);
         }
     }
 
@@ -71,37 +113,52 @@ class PessoaTipoController extends DeiaController{
 
     /**
      *
+     */
+    public function salvarEdit(){
+        try{
+            $tipo_cod = $_POST["tipo_cod"];
+            $tipo_nome = $_POST["tipo_nome"];
+
+            $this->checkNumeric($tipo_cod);
+            $this->checkString($tipo_nome);
+
+            if(PessoaTipoModel::update($tipo_cod, $tipo_nome)){
+                $this->splash("Tipo salvo com sucesso");
+                $this->go($this->urlController);
+            }else{
+                $this->splash("Tentativa falhou! Tente novamente");
+                $this->go($this->urlController);
+            }
+        }catch (InvalidArgumentException $e){
+            $this->splash($e->getMessage());
+            $this->go($this->urlController);
+        }
+    }
+
+    /**
+     *
      * @return type
      */
     public function grid(){
         try{
-            $grid = new DeiaGrid("email","Index");
+            $grid = new DeiaGrid("tipo_cod", $this->urlController, 10);
 
-            $grid->numRegisterTotal = 101;
-            $grid->urlPagination = "$this->urlApp"."Index/";
-            $grid->setColumnGrid(array("label"=>"#","field"=>"link","align"=>"center"));
-            $grid->setColumnGrid(array("label"=>"Cod","field"=>"cod","class"=>"","title"=>"Coluna do Códido"));
-            $grid->setColumnGrid(array("label"=>"Nome","field"=>"nome","class"=>"","title"=>""));
-            $grid->setColumnGrid(array("label"=>"Idade","field"=>"idade","class"=>"","title"=>""));
-            $grid->setColumnGrid(array("label"=>"Cargo","field"=>"cargo","class"=>"","title"=>""));
-            $grid->setColumnGrid(array("label"=>"Estato Civil","field"=>"estadoCivil","class"=>"","title"=>""));
-            $grid->setColumnGrid(array("label"=>"Célula","field"=>"celula","class"=>"","title"=>""));
-
-            for($i = 1; $i <= 100; $i++){
-                $grid->setDataGrid(
-                    array(
-                        DeiaHtml::a($this->urlApp."Index/Edit/".str_pad($i,4,"0",STR_PAD_LEFT), DeiaHtml::img($this->urlAppImages."edit_16x16.png")),
-                        "cod"=>str_pad($i,4,"0",STR_PAD_LEFT),
-                        "nome"=>"Rafael de Meira Teixeira",
-                        "idade"=>"33",
-                        "cargo"=>"Professor de EBD",
-                        "estadoCivil"=>"Casado",
-                        "celula"=>"Supervisor"
-                    )
-                );
+            if(empty($grid->postGrid["exibir"])){
+                $grid->numRegisterTotal = PessoaTipoModel::selectDataTotalRegister();
+                $grid->postGrid["exibir"] = $grid->numRegisterTotal;
+            }else{
+                $grid->numRegisterTotal = $grid->postGrid["exibir"];
             }
+            $grid->setColumnGrid(array("label"=>"TIPCOD","field"=>"link","align"=>"left"));
+            $grid->setColumnGrid(array("label"=>"Nome do Tipo","field"=>"tipo_nome","class"=>"","title"=>"Nome do Tipo"));
 
-            return $grid->gridFetch();
+            $gridMenu[] = array("href"=>$this->urlController."Edit/","label"=>"Editar","icon"=>"fa-edit");
+            $gridMenu[] = array("href"=>"#","label"=>"Remover","icon"=>"fa-archive","confirm"=>true,"location"=>$this->urlController."Delete");
+            $grid->gridMenu = $gridMenu;
+            $grid->dataGrid = PessoaTipoModel::selectData($grid->start, $grid->limit);
+
+            $this->show($grid->gridFetch());
+
         }catch(InvalidArgumentException $e){
             echo $e->getMessage();
         }
